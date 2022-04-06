efb93fa62d96f0f9c6971f5479a6f08e1f26a138
tzae <102624996+tzae@users.noreply.github.com>
2022-04-06 22:07:19 +0200

    clear useless.js

--- useless.js
+++
@@ -1,8 +0,0 @@
-const address = document.getElementById("address")
-const buttonaddress = document.getElementById("buttonaddress")
-buttonaddress.onclick = function () {
-
-    address.select();
-
-    document.execCommand(copyToClipboard)
-}
